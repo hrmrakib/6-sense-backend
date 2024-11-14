e51df759ba2a86e2899edb36e77b5ea3acaaf8a6
hrmrakib <hrmrakibs@gmail.com>
2024-11-15 00:02:22 +0600

    implement delete operation

---
+++ controllers/userController.js
@@ -0,0 +1,43 @@
+import { userCollection } from "../config/db.js";
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await userCollection.find().toArray();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const createNewUser = async (req, res) => {
+  try {
+    const { firstname, lastname, email, phone } = req.body;
+    const newUser = { firstname, lastname, email, phone };
+
+    const emailExist = await userCollection.findOne({ email });
+    if (emailExist) {
+      return res.send({ message: "Email already exist" });
+    }
+
+    const phoneExist = await userCollection.findOne({ phone });
+    if (phoneExist) {
+      return res.send({ message: "Phone number already exist" });
+    }
+
+    const result = await userCollection.insertOne(newUser);
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const result = await userCollection.deleteOne({ email });
+    res.send(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,36 +1,16 @@
 import express from "express";
-import { userCollection } from "../config/db.js";
+import {
+  createNewUser,
+  deleteUser,
+  getAllUsers,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await userCollection.find().toArray();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get("/users", getAllUsers);
 
-router.post("/create", async (req, res) => {
-  try {
-    const { firstname, lastname, email, phone } = req.body;
-    const newUser = { firstname, lastname, email, phone };
+router.post("/create", createNewUser);
 
-    console.log({ newUser });
-
-    const result = await userCollection.insertOne(newUser);
-
-    res.send(result);
-    // if (result.acknowledged) {
-    //   res.status(201).json({ message: "User created successfully" });
-    // }
-    //  else {
-    //   res.status(500).json({ message: "Failed to create user" });
-    // }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.delete("/delete/:email", deleteUser);
 
 export default router;
