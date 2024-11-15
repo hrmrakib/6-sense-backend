6ab852518be1a55698974b1f725645f28a56f7be
hrmrakib <hrmrakibs@gmail.com>
2024-11-15 08:49:08 +0600

    implement user update api

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,6 +32,30 @@ export const createNewUser = async (req, res) => {
   }
 };
 
+export const updateUser = async (req, res) => {
+  const { email } = req.params;
+  const { firstname, lastname, phone } = req.body;
+
+  const updateUserInfo = {
+    $set: {
+      firstname,
+      lastname,
+      phone,
+    },
+  };
+  try {
+    const updatedUser = await userCollection.updateOne(
+      {
+        email,
+      },
+      updateUserInfo
+    );
+    res.send(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 export const deleteUser = async (req, res) => {
   try {
     const { email } = req.params;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,7 @@ import {
   createNewUser,
   deleteUser,
   getAllUsers,
+  updateUser,
 } from "../controllers/userController.js";
 
 const router = express.Router();
@@ -10,6 +11,8 @@ const router = express.Router();
 router.get("/users", getAllUsers);
 
 router.post("/create", createNewUser);
+
+router.put("/update/:email", updateUser);
 
 router.delete("/delete/:email", deleteUser);
 
